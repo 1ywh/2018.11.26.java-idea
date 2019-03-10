9cf88fd558dc0c3041b9f2c12a4bd999d44692c2
1ywh <3356268629@qq.com>
2019-03-10 20:24:01 +0800

    自定义类加载器

---
+++ java-workspace/java_Lock/src/com/bittech/LoaderClass.java
@@ -0,0 +1,18 @@
+package com.bittech;
+
+/**自定义类加载器
+ * Author:ywh
+ * Date:2019/03/05
+ */
+
+class Member{
+    @Override
+    public String toString() {
+        return "Member";
+    }
+}
+public class LoaderClass {
+    public static void main(String[] args) throws Exception{
+        System.out.println(Class.forName("Member").getClassLoader().loadClass("Member").newInstance());
+    }
+}
