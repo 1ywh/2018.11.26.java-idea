e68cd2378269df2825625c5216f8319dce31faf5
1ywh <3356268629@qq.com>
2019-09-25 17:00:44 +0800

    继承

---
+++ 数据结构/normal/normal/src/TestSummary/ExtendsClass.java
@@ -0,0 +1,35 @@
+package TestSummary;
+
+/**
+ * @author ywh
+ * @date 2019/9/25 15:16
+ * @description  继承的应用,实例的时候先执行父类的构造方法
+ */
+class Parent {
+    private String name;
+    //如果不写这个的话，会报错
+    public Parent(){
+        System.out.println("o");
+    }
+
+    public void test(){
+        System.out.println(1);
+    }
+}
+class Student extends Parent{
+    public void test(){
+        System.out.println(2);
+    }
+    public Student(){
+
+        System.out.println(4);
+    }
+}
+public class ExtendsClass {
+    public static void main(String[] args) {
+        new Student().test();
+        System.out.println("=======");
+        //在这里构造方法优先于普通方法
+
+    }
+}
